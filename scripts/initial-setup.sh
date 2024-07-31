8b89810ed8ecae4ac082970da5704e5608856cc4
kestr31 <samsani@naver.com>
2024-08-01 03:07:21 +0900

    Initial setup script. draft.

---
+++ scripts/initial-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# INITIAL STATEMENTS
+# >>>----------------------------------------------------
+
+# SET THE BASIC ENVIRONMENT VARIABLE
+export TERM=xterm-256color
+
+# SET THE BASE DIRECTORY
+BASE_DIR=$(dirname $(readlink -f "$0"))
+REPO_DIR=$(dirname $(dirname $(readlink -f "$0")))
+
+# SOURCE THE ENVIRONMENT AND FUNCTION DEFINITIONS
+source ${BASE_DIR}/include/commonFcn.sh
+source ${BASE_DIR}/include/commonEnv.sh
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# MAIN STATEMENTS
+# >>>----------------------------------------------------
+
+PX4_WORKSPACE=${SITL_DEPLOY_DIR}/PX4-Autopilot
+GAZEBO_CLASSIC_WORKSPACE=${SITL_DEPLOY_DIR}/Gazebo-Classic
+AIRSIM_WORKSPACE=${SITL_DEPLOY_DIR}/AirSim
+ROS2_WORKSPACE=${SITL_DEPLOY_DIR}/ROS2
+QGC_WORKSPACE=${SITL_DEPLOY_DIR}/QGroundControl
+
+EchoYellow "[$(basename $0)] CONFIGUREING INITIAL CONDITIONS."
+
+# CLONING AND BUILDING PX4-AUTOPILOT
+${BASE_DIR}/run.sh px4 clone.sh
+${BASE_DIR}/run.sh px4 build.sh
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
