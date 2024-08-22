97cafe8a677578b048e6785af69a70712350bc66
kestr31 <samsani@naver.com>
2024-08-23 04:05:05 +0900

    Moved per-container workspace definitions to env definition. Added gazebo headless mode control variable

--- scripts/include/commonEnv.sh
+++ scripts/include/commonEnv.sh
@@ -1,8 +1,19 @@
 # COMMON ENVIRONMENT VARIABLES
 ## THIS VARIABLES WILL BE USED FOR SCRIPT CONTROL
 # >>>----------------------------------------------------
-
 SITL_DEPLOY_DIR=${HOME}/Documents/A4VAI-SITL
+GAEZBO_HEADLESS=true
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# DEPENDENT ENVIRONMENT VARIABLES
+## DO NOT MODIFY THESE VARIABLES UNLESS YOU KNOW WHAT YOU ARE DOING
+# >>>----------------------------------------------------
 SITL_ENV_DIR=${SITL_DEPLOY_DIR}/envs
 
+PX4_WORKSPACE=${SITL_DEPLOY_DIR}/PX4-Autopilot
+GAZEBO_CLASSIC_WORKSPACE=${SITL_DEPLOY_DIR}/Gazebo-Classic
+AIRSIM_WORKSPACE=${SITL_DEPLOY_DIR}/AirSim
+ROS2_WORKSPACE=${SITL_DEPLOY_DIR}/ROS2
+QGC_WORKSPACE=${SITL_DEPLOY_DIR}/QGroundControl
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
