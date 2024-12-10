28a376c222e949561bf501e73fc223b1fba3c84f
kestr31 <samsani@naver.com>
2024-12-11 00:29:51 +0900

    Introduced Rosboard for easier debugging

--- scripts/ros2/sitl-px4-airsim.sh
+++ scripts/ros2/sitl-px4-airsim.sh
@@ -71,6 +71,8 @@ ros2 run plotter plot 2>&1 | tee ${WORKSPACE_DIR}/logs/plot.log &
 # LOGGING ALL TOPICS TO BAG FILE
 ros2 bag record -a -o ${WORKSPACE_DIR}/logs/rosbag 2>&1 | tee ${WORKSPACE_DIR}/logs/plot.log &
 
+${WORKSPACE_DIR}/rosboard/run
+
 # PLACE USER-DEFINED SHELL SCRIPTS/COMMANDS HERE
 # FOR EXAMPLE FOR RUNNING:
 #   algorithm1 build at /home/user/workspace/ros2/alg_ws
