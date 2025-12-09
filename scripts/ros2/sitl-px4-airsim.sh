27e7eb1c6ae33b57183c4be4716b96cf3cc6613a
JOCIIIII <jociiiii@inha.edu>
2025-12-09 22:37:05 +0900

    update run file

--- scripts/ros2/sitl-px4-airsim.sh
+++ scripts/ros2/sitl-px4-airsim.sh
@@ -43,37 +43,46 @@ ${BASE_DIR}/uxrce-dds.sh 2>&1 | tee ${WORKSPACE_DIR}/logs/uxrce-dds.log &
 ${BASE_DIR}/airsim-bridge.sh 2>&1 | tee ${WORKSPACE_DIR}/logs/airsim-bridge.log &
 
 CheckDirExists ${WORKSPACE_DIR}/ros2_ws/build/controller/controller/log create
+pip install transforms3d
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-# USER-DEFINED SATEMENTS
-# >>>----------------------------------------------------
-touch ${WORKSPACE_DIR}/logs/pcl_practice.log
-touch ${WORKSPACE_DIR}/logs/lidar_processing.log
-#rviz2&
 
-#ros2 launch lidar_processing run.launch.py 2>&1 | tee ${WORKSPACE_DIR}/logs/lidar_processing.log &
-#ros2 run pcl_practice pcl_node 2>&1 | tee ${WORKSPACE_DIR}/logs/pcl_practice.log &
-ros2 run dcm_test dcm_test
+# CREATE LOG FILES
+touch ${WORKSPACE_DIR}/logs/controller.log
+touch ${WORKSPACE_DIR}/logs/rs_converter.log
+touch ${WORKSPACE_DIR}/logs/tf2_ros.log
+touch ${WORKSPACE_DIR}/logs/direct_infer.log
+touch ${WORKSPACE_DIR}/logs/point_cloud_filter_node.log
+touch ${WORKSPACE_DIR}/logs/point_cloud_feature_extractor_cov.log
 
-#touch ${WORKSPACE_DIR}/logs/controller.log
-#touch ${WORKSPACE_DIR}/logs/pathplanning.log
-#touch ${WORKSPACE_DIR}/logs/collision_avoidance.log
-#touch ${WORKSPACE_DIR}/logs/pub_depth.log
-#touch ${WORKSPACE_DIR}/logs/node_MPPI_output.log
-#touch ${WORKSPACE_DIR}/logs/node_att_ctrl.log
-#touch ${WORKSPACE_DIR}/logs/plot.log
-#touch ${WORKSPACE_DIR}/logs/lidar.log
+touch ${WORKSPACE_DIR}/logs/foxglove.log
+touch ${WORKSPACE_DIR}/logs/foxglove_bridge.log
+touch ${WORKSPACE_DIR}/logs/tf2_ros.log
+touch ${WORKSPACE_DIR}/logs/node_MPPI_output.log
+touch ${WORKSPACE_DIR}/logs/node_att_ctrl.log
+touch ${WORKSPACE_DIR}/logs/Plan2WP.log
 
-#ros2 run controller controller 2>&1 | tee ${WORKSPACE_DIR}/logs/controller.log &
-#ros2 run pathplanning Plan2WP 2>&1 | tee ${WORKSPACE_DIR}/logs/pathplanning.log &
-#ros2 run collision_avoidance collision_avoidance 2>&1 | tee ${WORKSPACE_DIR}/logs/collision_avoidance.log &
-#ros2 run pub_depth pub_depth 2>&1 | tee ${WORKSPACE_DIR}/logs/pub_depth.log &
-#ros2 run pathfollowing node_MPPI_output 2>&1 | tee ${WORKSPACE_DIR}/logs/node_MPPI_output.log &
-#ros2 run pathfollowing node_att_ctrl 2>&1 | tee ${WORKSPACE_DIR}/logs/node_att_ctrl.log &
 
-#ros2 run plotter plot 2>&1 | tee ${WORKSPACE_DIR}/logs/plot.log &
-#ros2 run lidar lidar & 2>&1 | tee ${WORKSPACE_DIR}/logs/lidar.log &
-#ros2 run lidar check_collision & 2>&1 | tee ${WORKSPACE_DIR}/logs/lidar.log &
-# ros2 run lidar plot & 2>&1 | tee ${WORKSPACE_DIR}/logs/lidar.log &
+
+ros2 run pathplanning Plan2WP 2>&1 | tee ${WORKSPACE_DIR}/logs/Plan2WP.log &
+
+ros2 run pathfollowing node_MPPI_output 2>&1 | tee ${WORKSPACE_DIR}/logs/node_MPPI_output.log &
+ros2 run pathfollowing node_att_ctrl 2>&1 | tee ${WORKSPACE_DIR}/logs/node_att_ctrl.log &
+
+# RUN THE ROS2 NODES
+ros2 run rs_converter rs_converter 2>&1 | tee ${WORKSPACE_DIR}/logs/rs_converter.log &
+ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 SimpleFlight/RPLIDAR_A3 base_link 2>&1 | tee ${WORKSPACE_DIR}/logs/tf2_ros.log &
+ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link velodyne 2>&1 | tee ${WORKSPACE_DIR}/logs/tf2_ros.log &
+# ros2 run lidar lidar 2>&1 | tee ${WORKSPACE_DIR}/logs/lidar.log &
+
+ros2 run controller controller 2>&1 | tee ${WORKSPACE_DIR}/logs/controller.log &
+
+ros2 run rs_converter point_cloud_filter_node 2>&1 | tee ${WORKSPACE_DIR}/logs/point_cloud_filter_node.log &
+ros2 run rs_converter point_cloud_feature_extractor_cov 2>&1 | tee ${WORKSPACE_DIR}/logs/point_cloud_feature_extractor_cov.log &
+
+python3 /home/user/workspace/ros2/ros2_ws/src/Python_version_easy/direct_infer.py --ros-args   -p model_path:=/home/user/workspace/ros2/ros2_ws/src/Python_version_easy/walid.onnx   -p vec_normalize_path:=/home/user/workspace/ros2/ros2_ws/src/Python_version_easy/vec_normalize.pkl   -p pointcloud_topic:=/pointcloud_features   -p cmd_topic:=/ca_vel_2_control  -p expected_points:=256 2>&1 | tee ${WORKSPACE_DIR}/logs/direct_infer.log &
+
+ros2 run foxglove foxglove 2>&1 | tee ${WORKSPACE_DIR}/logs/foxglove.log &
+ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765 2>&1 | tee ${WORKSPACE_DIR}/logs/foxglove_bridge.log &
 
 # RUN ROSBOARD FOR ROS2 TOPIC VISUALIZATION
 ${WORKSPACE_DIR}/rosboard/run
